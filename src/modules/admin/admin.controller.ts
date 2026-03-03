f5af4a77be3f935825364fda815174d695737c6b
Aarif <adebayoabdulazeez195@gmail.com>
2026-03-03 15:01:29 +0100

    use adminRouteResponseDto for global response

--- src/modules/admin/admin.controller.ts
+++ src/modules/admin/admin.controller.ts
@@ -1,35 +1,49 @@
-import { Body, Controller, Post, HttpCode, HttpStatus, UseGuards } from '@nestjs/common';
-import { ApiOperation, ApiResponse, ApiTags, ApiBody } from '@nestjs/swagger';
+import {
+  Body,
+  Controller,
+  Post,
+  HttpCode,
+  HttpStatus,
+  UseGuards,
+} from '@nestjs/common';
+import {
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+  ApiBody,
+  ApiUnauthorizedResponse,
+  ApiOkResponse,
+} from '@nestjs/swagger';
 import { AdminService } from './admin.service';
-import { AdminLoginDto } from './dto/admin-login.dto';
+
 import { AuthGuard } from '@/common/guards/auth.guard';
 import { RolesGuard } from '@/common/guards/roles.guard';
 import { Roles } from '@/common/decorators/roles.decorator';
+
+import { AdminLoginDto } from './dto/admin-login.dto';
+import { AdminResponseDto } from './dto/admin-response.dto';
+import { ErrorResponseDto } from '@/common/dto/error-response.dto';
 
 @ApiTags('Admin Dashboard')
 @Controller('admin')
 // @UseGuards(AuthGuard, RolesGuard)
 @Roles('superadmin')
 export class AdminController {
-  constructor(private readonly adminService: AdminService) { }
+  constructor(private readonly adminService: AdminService) {}
 
   // login
   @Post('login')
   @HttpCode(HttpStatus.OK)
   @ApiOperation({ summary: 'Admin login' })
   @ApiBody({ type: AdminLoginDto })
-  @ApiResponse({
-    status: 200,
+  @ApiOkResponse({
     description: 'Login successful',
-    schema: {
-      example: {
-        message: 'Admin login successful',
-        accessToken: 'eyJhbG...',
-        admin: { id: 1, email: 'admin@hostel.com', role: 'superadmin' }
-      }
-    }
+    type: AdminResponseDto,
   })
-  @ApiResponse({ status: 401, description: 'Invalid credentials' })
+  @ApiUnauthorizedResponse({
+    description: 'Invalid credentials',
+    type: ErrorResponseDto,
+  })
   async login(@Body() dto: AdminLoginDto) {
     return this.adminService.login(dto);
   }
